43c5cb3ae20d5d11db4ccea3616158e921c3b696
Ecem Günenç <ecem.gunenc@mskglobal.net>
2025-12-11 20:24:36 +0300

    host fix

--- employee-service/src/main/java/io/github/ecemgc/employeeservice/security/WebConfig.java
+++ employee-service/src/main/java/io/github/ecemgc/employeeservice/security/WebConfig.java
@@ -1,5 +1,6 @@
 package io.github.ecemgc.employeeservice.security;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,10 +8,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    @Value("${appConfig.uiHost}")
+    private String uiHost;
+
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins(uiHost)
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true)
